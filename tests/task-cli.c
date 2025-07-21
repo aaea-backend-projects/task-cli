78bc3bf605d6674c8087077ff376ab662b2046e5
ago-agere-egi-actum <jl.software.engineering@gmail.com>
2025-07-21 20:09:23 -0300

    Tests function for showing usage.

---
+++ tests/task-cli.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "../src/utils.h"
+
+int test_show_usage() {
+    char * expected = "./task-cli [action] [options]\n";
+    char * output = show_usage();
+    return (strcmp(output, expected) == 0)? 1 : 0;
+}
+
+int main() {
+    printf("==== STARTING TESTS FOR TASK-CLI ====\n");
+    int total_tests = 1;
+    int total_ok_tests = 0;
+
+    test_fn(&test_show_usage);
+
+    total_ok_tests++;
+
+    printf("==== END TESTS FOR TASK-CLI ====\n");
+    printf("Passed %d of %d\n", total_ok_tests, total_tests);
+}

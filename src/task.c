73c007cf028176554c32b64c71f265fe33e2e76e
ago-agere-egi-actum <jl.software.engineering@gmail.com>
2025-07-21 21:18:31 -0300

    task structure and create a task function.

---
+++ src/task.c
@@ -0,0 +1,6 @@
+#include "task.h"
+
+task_t create_task() {
+    task_t t = {.id=1};
+    return t;
+}
---
+++ src/task.h
@@ -0,0 +1,10 @@
+#ifndef TASK_H
+#define TASk_H
+
+
+typedef struct {
+    int id;
+} task_t;
+
+task_t create_task();
+#endif

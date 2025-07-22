bfb35adca5c6ba1795d4fc5637418ccd336a0a2b
ago-agere-egi-actum <jl.software.engineering@gmail.com>
2025-07-21 21:17:48 -0300

    test: can create a task.

--- tests/task-cli.c
+++ tests/task-cli.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "../src/utils.h"
+#include "../src/task.h"
 
 int test_show_usage() {
     char * expected = "./task-cli [action] [options]\n";
@@ -8,13 +9,23 @@ int test_show_usage() {
     return (strcmp(output, expected) == 0)? 1 : 0;
 }
 
+int test_create_one_task() {
+    task_t task = create_task();
+    if(task.id == 1) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
     printf("==== STARTING TESTS FOR TASK-CLI ====\n");
-    int total_tests = 1;
+    int total_tests = 2;
     int total_ok_tests = 0;
 
     test_fn(&test_show_usage);
+    total_ok_tests++;
 
+    test_fn(&test_create_one_task);
     total_ok_tests++;
 
     printf("==== END TESTS FOR TASK-CLI ====\n");
